3d56e6e0c8d322da386de98ba028da308beb13f7
Booharin <booharin@bk.ru>
2020-01-30 17:44:08 +0300

    Updated task_3

--- task_3.py
+++ task_3.py
@@ -5,6 +5,15 @@ class MyCage:
     def __add__(self, other):
         return MyCage(self.cells + other.cells)
 
+    def __sub__(self, other):
+        return MyCage(self.cells - other.cells)
+
+    def __mul__(self, other):
+        return MyCage(self.cells * other.cells)
+
+    def __floordiv__(self, other):
+        return MyCage(self.cells / other.cells)
+
 
 c_1 = MyCage(4)
 print(c_1.cells)
@@ -12,5 +21,5 @@ print(c_1.cells)
 c_2 = MyCage(7)
 print(c_2.cells)
 
-c_3 = c_1 + c_2
+c_3 = c_1 / c_2
 print(c_3.cells)
